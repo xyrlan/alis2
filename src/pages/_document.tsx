4e651d6b6976eaf81b7966b00c6fa265d3136030
xyrlan <lobatopedro68@gmail.com>
2026-04-04 15:40:19 -0300

    Refactor _document.tsx for improved readability and consistency
    - Reformatted the preconnect link for fonts.gstatic.com for better code clarity.
    - Removed unnecessary blank line to streamline the document structure.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,15 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body
         suppressHydrationWarning
-        className="antialiased overflow-hidden text-white selection:bg-white selection:text-black"
+        className="antialiased text-white selection:bg-white selection:text-black"
       >
         <Main />
         <NextScript />
@@ -17,4 +21,3 @@ export default function Document() {
     </Html>
   );
 }
-
